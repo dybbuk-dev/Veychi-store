7a9ca0d33c4955fa64698f73764b1504d433784e
dybbuk555 <dybbuk555@gmail.com>
2022-10-20 17:31:13 -0300

    fix

--- admin/rest/src/repositories/base.ts
+++ admin/rest/src/repositories/base.ts
@@ -1,4 +1,4 @@
-import http from "@utils/api/http";
+import http from '@utils/api/http';
 
 export default class Base<C, U> {
   http = async <T>(
@@ -10,28 +10,28 @@ export default class Base<C, U> {
     return (http as any)[type](url, variables, options);
   };
   all = async (url: string) => {
-    return this.http(url, "get");
+    return this.http(url, 'get');
   };
 
   find = async (url: string) => {
-    return this.http(url, "get");
+    return this.http(url, 'get');
   };
 
   create = async (url: string, variables: C, activationToken?: string) => {
-    if (!activationToken) return this.http<C>(url, "post", variables);
-    const { data } = await this.http<C>(url, "post", variables);
+    if (!activationToken) return this.http<C>(url, 'post', variables);
+    const { data } = await this.http<C>(url, 'post', variables);
     await this.http<C>(
       (process.env.NEXT_PUBLIC_REST_API_ENDPOINT as string) +
-        `/shop/${data.id}/aprove?token=${activationToken}`,
-      "post"
+        `shop/${data.id}/aprove?token=${activationToken}`,
+      'post'
     );
   };
 
   update = async (url: string, variables: U) => {
-    return this.http<U>(url, "put", variables);
+    return this.http<U>(url, 'put', variables);
   };
 
   delete = async (url: string) => {
-    return this.http(url, "delete");
+    return this.http(url, 'delete');
   };
 }
