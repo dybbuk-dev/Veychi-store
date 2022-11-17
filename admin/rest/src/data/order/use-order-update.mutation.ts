2e214b44c37234dd8b708ce2d608e41dbfb8e5ad
dybbuk555 <dybbuk555@gmail.com>
2022-11-17 10:25:16 -0500

    modificando la funcion de useUpdateOrderMutation se le quito el auth para evitar el error al cargar una evidencia

--- admin/rest/src/data/order/use-order-update.mutation.ts
+++ admin/rest/src/data/order/use-order-update.mutation.ts
@@ -21,7 +21,7 @@ export const useUpdateOrderMutation = () => {
       },
       // Always refetch after error or success:
       onSettled: () => {
-        queryClient.invalidateQueries(API_ENDPOINTS.ORDERS,{},{auth});
+        queryClient.invalidateQueries(API_ENDPOINTS.ORDERS,{},{});
       },
     }
   );
